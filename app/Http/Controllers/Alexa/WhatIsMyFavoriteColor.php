6d4f5059960c8e4e91268ec411866099a172e9f4
Rasmus Christoffer Nielsen <rasmus@easify.dk>
2016-12-24 01:15:34 +0100

    Initial commit

---
+++ app/Http/Controllers/Alexa/WhatIsMyFavoriteColor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Alexa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WhatIsMyFavoriteColor extends Controller
+{
+    public function handle()
+    {
+        dd('ok');
+    }
+}
